35f974aa7dc705c1b0500855d610a887da41f94a
Z1CCO <xzikriloxon@gmail.com>
2024-05-17 14:12:47 +0500

    test

--- lib/saving/user.dart
+++ lib/saving/user.dart
@@ -1,23 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Users {
-  final String id;
+  final String userid;
   final String username;
   final String email;
+  final String password;
 
   final bool admin;
 
   Users({
-    required this.id,
+    required this.userid,
     required this.username,
     required this.email,
+    required this.password,
     required this.admin,
   });
 
   factory Users.fromDocument(DocumentSnapshot doc) {
     return Users(
-      id: doc['id'],
+      userid: doc['userid'],
       username: doc['username'],
+      password: doc['password'],
       email: doc['email'],
       admin: doc['admin'],
     );

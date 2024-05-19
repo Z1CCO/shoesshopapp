4cf0c021d54c5ca94b3610a6083b6cf0e372a53a
erkinovsdev <muhammadaminrumiy@gmail.com>
2024-05-19 14:37:20 +0300

    container

--- lib/ui/homescreen.dart
+++ lib/ui/homescreen.dart
@@ -25,6 +25,11 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: Text('Products'),
+        actions: [Icon(Icons.search)],
+      ),
       body: Stack(
         children: [
           _widgetOptions[_selectedIndex],
@@ -38,8 +43,40 @@ class _HomeScreenState extends State<HomeScreen> {
                 color: Colors.black,
                 borderRadius: BorderRadius.circular(50),
               ),
-              child: Row(
-                children: [],
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.home_outlined,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 135.0),
+                      child: Icon(
+                        Icons.favorite_outline,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (context) => Profile()),
+                          (Route<dynamic> route) => false,
+                        );
+                      },
+                      child: Icon(
+                        Icons.person_outline_rounded,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
